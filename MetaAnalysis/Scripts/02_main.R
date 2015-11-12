16110a02f03d50e32b42c446e3fdbab27a0f7c72
Helen Phillips <helen.r.phillips@googlemail.com>
2015-11-12 15:33:31 +0000

    Adding code for map

--- MetaAnalysis/Scripts/02_main.R
+++ MetaAnalysis/Scripts/02_main.R
@@ -1,11 +1,64 @@
+#################
+## Functions
+#################
 source("~/Dropbox/PhD_Copy/Wildlife Garden/MetaAnalysis/Scripts/Functions/GoogleSpreadsheets.R")
 source("~/Dropbox/PhD_Copy/Wildlife Garden/MetaAnalysis/Scripts/Functions/CheckComparisons.R")
 
+#################
+## Libraries
+#################
+
+library(lme4)
+library(maps)
+library(mapdata)
+library(maptools)
+library(MuMIn)
+library(dplyr)
+
+
+#################
+## Locations
+#################
+
+figure_out <- "~/Dropbox/PhD_Copy/Wildlife Garden/MetaAnalysis/Scripts/Figures"
+
+#################
+## Data
+#################
+
 garden <- OpenGS()
 
+
+################################
 
 comparisons <- check_comparisons(garden)
 pdf(file = "~/Dropbox/PhD_Copy/Wildlife Garden/MetaAnalysis/Scripts/Figures/HabitatComparisons.pdf")
 comparisons[[2]]
 dev.off()
+
+#################
+## Study info 
+#################
+
+length(unique(garden$Study.ID)) ## 28
+
+
+
+#################
+## MAP
+#################
+coord<-aggregate(cbind(garden$long, garden$lat), list(garden$Study.ID), max)
+coord$X<-coord$Group.1
+coord<-coord[2:4]
+names(coord)<-c("Long", "Lat", "X")
+dsSPDF<-SpatialPointsDataFrame(coord[,1:2], data.frame(coord[,1:3]))
+proj4string(dsSPDF)<-CRS("+proj=longlat")
+
+png(file.path(figure_out, "Map.png"), pointsize=11)
+par(mar=c(0, 0, 0, 0))
+map('worldHires', c("UK"),border=0,fill=TRUE, col="forestgreen",  xlim=c(-8,2), ylim=c(49,60.9), mar = c(0, 0, 0, 0))
+points(dsSPDF, col="black", bg="black", cex= 1.5, pch=19)
+text(-7, 49.5, "Figure 1 \nMap of the study locations", pos=4)
+dev.off()
+
 
