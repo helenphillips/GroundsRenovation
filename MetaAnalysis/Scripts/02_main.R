9cba6d3e928d68a447d389f6ddbd3afe4c597e8c
Helen Phillips <helen.r.phillips@googlemail.com>
2015-12-04 15:22:22 +0000

    slight tweaks to output table

--- MetaAnalysis/Scripts/02_main.R
+++ MetaAnalysis/Scripts/02_main.R
@@ -88,7 +88,7 @@ garden$SpeciesDensity <- ifelse(is.na(garden$Sampled.Area_metres), FALSE, TRUE)
 
 ## Calculate a species density for when an entire area has been sampled.
 ## As species density varies within fragment area
-garden$Corrected_Taxon.Richness <- ifelse(is.na(garden$Sampled.Area_metres) & !(is.na(garden$Habitat.Area_metres)), calculate_density_cSAR(S=garden$Taxon.Richness, A=garden$Habitat.Area_metres, x=0.1, newA=10), garden$Corrected_Taxon.Richness)
+garden$Corrected_Taxon.Richness <- ifelse(is.na(garden$Sampled.Area_metres) & !(is.na(garden$Habitat.Area_metres)), calculate_density_cSAR(S=garden$Taxon.Richness, A=garden$Habitat.Area_metres, z=0.1, newA=10), garden$Corrected_Taxon.Richness)
 
 garden$SpeciesDensity <- ifelse(is.na(garden$Sampled.Area_metres) & !(is.na(garden$Habitat.Area_metres)), TRUE, garden$SpeciesDensity)
 
@@ -111,8 +111,9 @@ garden$Study.ID[garden$Corrected_Taxon.Richness > 100] ## Because its cubic metr
 #########
 sampled_area <- garden[garden$SpeciesDensity == TRUE,] ## 371
 sampled_area <- sampled_area[sampled_area$Taxonimic.Level == "Species",] ## 367
-sampled_area <- droplevels(sampled_area)
 sampled_area <- sampled_area[complete.cases(sampled_area$Taxon.Richness),]## 345
+sampled_area <- droplevels(sampled_area)
+
 
 table(sampled_area$Habitat, sampled_area$Study.ID)
 table(sampled_area$Habitat)
@@ -121,19 +122,16 @@ not_enough <- c("green roof", "Not present in NHM", "species-poor hedgerow", "Un
 
 sampled_area <- sampled_area[!(sampled_area$Habitat %in% not_enough),] ## 327
 
+## Need studies that have sampled more than one habitat
 sample_studies <- as.data.frame(aggregate(sampled_area$Habitat, list(sampled_area$Study.ID), function(x){N = length(unique(x, na.rm=TRUE))}))
 sample_studies <- sample_studies[sample_studies$x > 1,]
 ## The studies with only one habitat in are amenity grasslands (pretty much), so not losing anything by removing
-
 sampled_area <- sampled_area[sampled_area$Study.ID %in% sample_studies$Group.1,] ## 182
 sampled_area <- droplevels(sampled_area)
 
 table(sampled_area$Habitat)
-
 tapply(sampled_area$Corrected_Taxon.Richness, sampled_area$Habitat, summary)
-
 table(sampled_area$Study.ID, sampled_area$Habitat)
-
 table(sampled_area$Study.ID, sampled_area$Taxa)
 
 exclude_these_studies <- c("2003_Thompson 1", "2006_SmithA 1", "2006_SmithA 2")
@@ -150,15 +148,14 @@ levels(taxa)[levels(taxa) != "Plants"] <- "Inverts"
 table(sampled_area$Habitat, taxa)
 density <- round(sampled_area$Corrected_Taxon.Richness)
 
-density1 <- glmer(density ~ Habitat * taxa + (1|Study.ID), data = sampled_area, family = poisson) ## Not convinved there is enough data for this model yet
+## Not convinved there is enough data for this model yet
+# density1 <- glmer(density ~ Habitat * taxa + (1|Study.ID), data = sampled_area, family = poisson) 
+
 density2 <- glmer(density ~ Habitat + taxa + (1|Study.ID), data = sampled_area, family = poisson)
 density3 <- glmer(density ~ Habitat + (1|Study.ID), data = sampled_area, family = poisson)
 anova(density2, density3)
 summary(density3)
 model_plot(density3) ## That's ok
-
-
-
 
 density3_means <- model_Means(density3)
 png(file.path(figure_out, "Habitat_density.png"), pointsize=11)
@@ -201,10 +198,10 @@ Proposed_area_m2=c(3267, 82, 526, 2141, 134, 122, 460, 83, 0, 159, 736, 518, 104
 )
 
 habitat_areas$Habitat <- tolower(habitat_areas$Habitat)
-
-tolower(density3_means$Habitat) %in% habitat_areas$Habitat
-# Check this everytime
 density3_means$Habitat <- tolower(density3_means$Habitat)
+density3_means$Habitat %in% habitat_areas$Habitat
+# Check this everytime
+
 habitat_areas <- merge(habitat_areas, density3_means, by.x = "Habitat", by.y = "Habitat", all.x = TRUE)[,1:4]
 
 names(habitat_areas)[4] <- "SpeciesDensity_10m2"
@@ -231,8 +228,8 @@ habitat_areas$DensityCorrectionFormula <- "exp(c*log(Area)^z)"
 
 
 ## Calculating the biodiversity change between the two
-habitat_areas$Corrected_SpeciesDensity_Current <- calculate_density_cSAR(S=habitat_areas$SpeciesDensity_10m2, A=10, x=0.1, newA=habitat_areas$Current_area_m2)
-habitat_areas$Corrected_SpeciesDensity_Proposed <- calculate_density_cSAR(S=habitat_areas$SpeciesDensity_10m2, A=10, x=0.1, newA=habitat_areas$Proposed_area_m2)
+habitat_areas$Corrected_SpeciesDensity_Current <- calculate_density_cSAR(S=habitat_areas$SpeciesDensity_10m2, A=10, z=0.1, newA=habitat_areas$Current_area_m2)
+habitat_areas$Corrected_SpeciesDensity_Proposed <- calculate_density_cSAR(S=habitat_areas$SpeciesDensity_10m2, A=10, z=0.1, newA=habitat_areas$Proposed_area_m2)
 
 
 ### Area weights
@@ -249,8 +246,7 @@ habitat_areas$Proposed_Weighted_density <- habitat_areas$SpeciesDensity_10m2 * h
 habitat_areas$Proposed_Weighted_Correcteddensity <- habitat_areas$Corrected_SpeciesDensity_Proposed * habitat_areas$Proposed_weight
 
 ## Totals
-habitat_areas[20,2:ncol(habitat_areas)] <- NA
-habitat_areas[20, c(2:3, 10:15)] <- colSums(habitat_areas[,c(2:3, 10:15)], na.rm = TRUE)
+habitat_areas[20, c(2: 12)] <- colSums(habitat_areas[,c(2:12)], na.rm = TRUE)
 
 write.csv(habitat_areas, file = "~/Dropbox/PhD_Copy/Wildlife Garden/MetaAnalysis/Scripts/Table/Habitat_totals.csv", row.names = FALSE)
 
@@ -265,7 +261,7 @@ png(file.path(figure_out, "Habitat_density_plusmissing.png"), pointsize=11)
 	labs2 <- c(labs, missing_coefs)
 	par(mar=c(14, 4, 1, 1))
 	errbar(1:nrow(density3_means), exp(density3_means[,2]), exp(density3_means[,3]), exp(density3_means[,4]), 
-		col = "white", main = "", sub ="", xlab ="", bty = "n", pch = 19, xaxt = "n", ylim=c(0,50), las = 1, cex= 1, ylab = "", xlim=c(0, length(labs2)))
+		col = "white", main = "", sub ="", xlab ="", bty = "n", pch = 19, xaxt = "n", ylim=c(0,40), las = 1, cex= 1, ylab = "", xlim=c(0, length(labs2)))
 	points(1:nrow(density3_means),exp(density3_means[,2]),col="black",bg="white",pch=19,cex=1)
 	missing_densities <- habitat_areas$SpeciesDensity_10m2[habitat_areas$Habitat %in% missing_coefs]
 	points((nrow(density3_means)+1):(length(labs2)), missing_densities, col="red", pch=19)
